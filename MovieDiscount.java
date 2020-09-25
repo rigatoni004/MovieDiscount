92a07f7ec9c0b9c89b1df9285fcc8f7657d3b3df
Casey Devet <casey.devet@sccdsb.net>
2020-09-25 08:40:43 -0400

    Added starter code.

---
+++ MovieDiscount.java
@@ -0,0 +1,25 @@
+/*
+ * Programmer:
+ * Date:
+ * Purpose:
+ */
+
+import java.util.Scanner;
+
+public class MovieDiscount {
+    public static void main (String [] args) {
+        // Create the input scanner
+        Scanner input = new Scanner(System.in);
+
+        // Ask the user for the side lengths
+        System.err.print("How old are you? ");
+        int age = input.nextInt();
+
+        // Ask the user if they have a coupon
+        System.err.print("Do you have a coupon? [true/false] ");
+        // TODO: Read in a true/false value
+
+        // Print the cost of a movie ticket
+        // TODO: Figure out the cost and print it
+    }
+}
