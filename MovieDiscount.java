d0f4fd431b696e3c7abe4e42807088feb16c1a5c
rigatoni004 <71712533+rigatoni004@users.noreply.github.com>
2020-10-05 12:12:13 -0400

    Update MovieDiscount.java

--- MovieDiscount.java
+++ MovieDiscount.java
@@ -21,7 +21,7 @@ public class MovieDiscount {
         boolean coupon = input.nextBoolean();
         
         // Print the cost of a movie ticket
-        if (age <= 13 || age >= 65) {
+        if (age < 13 || age > 65) {
         	if (coupon == true) { 
         		System.out.println("Your ticket costs $9.5."); 	
         	}
